3c69aaf30e2944535b3ac7c831f09958937a65aa
hey-d <95525086+hey-d@users.noreply.github.com>
2025-08-13 12:00:52 +0530

    Update socket.js
    updated server addition

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,5 +1,5 @@
 // src/utils/socket.js
 import { io } from 'socket.io-client';
-const URL = import.meta.env.VITE_API_URL || 'https://live-poll-backend-6z5n.onrender.com';
+const URL = 'https://live-poll-backend-6z5n.onrender.com';
 const socket = io(URL, { autoConnect: true });
 export default socket;
